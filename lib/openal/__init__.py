dbf8289d6a4d2778a546caac12ac38ca6b942e93
Marcus von Appen <marcus@sysfault.org>
2012-05-30 22:48:46 +0200

    - fixed openal-soft DLL for Windows x64 platforms

--- lib/openal/__init__.py
+++ lib/openal/__init__.py
@@ -6,17 +6,6 @@ import sys
 import ctypes
 from pygame2.dll import DLL
 
-_LIBNAME = None
-_FALLBACK = None
-if sys.platform in ("win32", "cli"):
-    _LIBNAME = "OpenAL32.dll"
-    _FALLBACK = "soft_oal.dll"
-elif sys.platform == "darwin":
-    _LIBNAME = "OpenAL"
-else:
-    _LIBNAME = "openal"
-
-dll = None
 dll = DLL("OpenAL", {"win32" : ["OpenAL", "OpenAL32", "soft_oal"],
                      "darwin" : ["OpenAL"],
                      "DEFAULT" : ["openal"]}
